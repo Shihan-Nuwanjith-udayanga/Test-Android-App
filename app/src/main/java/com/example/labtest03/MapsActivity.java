d9d43eba76b00c0039739b2c623797d3af476a7c
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2023-02-26 21:04:19 +0530

    Get the Longitude and Latitude of selected places from Google Maps using Geocoding.

--- app/src/main/java/com/example/labtest03/MapsActivity.java
+++ app/src/main/java/com/example/labtest03/MapsActivity.java
@@ -2,7 +2,10 @@ package com.example.labtest03;
 
 import androidx.fragment.app.FragmentActivity;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -12,10 +15,13 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.labtest03.databinding.ActivityMapsBinding;
 
+import java.util.List;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
+    List<Address> listGeoCoder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +34,22 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        try {
+            listGeoCoder = new Geocoder(this).getFromLocationName
+                    ("Akmeemana, Galle, SriLanka", 1);
+
+            double longitude = listGeoCoder.get(0).getLongitude();
+            double latitude = listGeoCoder.get(0).getLatitude();
+            String countryName = listGeoCoder.get(0).getCountryName();
+
+            Log.i("GOOGLE_MAP_TAG", "longitude is : "+ String.valueOf(longitude));
+            Log.i("GOOGLE_MAP_TAG", "Latitude is : "+ String.valueOf(latitude));
+            Log.i("GOOGLE_MAP_TAG", "Country is: "+ String.valueOf(countryName));
+
+        }catch (Exception exception){
+            exception.printStackTrace();
+        }
     }
 
     /**
@@ -41,12 +63,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+        /*mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
         LatLng galle = new LatLng(6.0343813717254084, 80.21654526534354);
         mMap.addMarker(new MarkerOptions().position(galle).title("Marker in Galle, Sri Lanka"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(galle,18.0f));
-        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(galle, 18.0f));
+        mMap.getUiSettings().setZoomControlsEnabled(true);*/
     }
 }
